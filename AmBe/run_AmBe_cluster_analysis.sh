223ed367723904bc9f0e3b896a56536408bf908d
lmlepin9 <lml24d@fsu.edu>
2026-03-06 14:16:11 -0800

    Adding a bash script to execute AmBe cluster jobs

---
+++ AmBe/run_AmBe_cluster_analysis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script to run DBScan cluster on AmBe files.
+
+
+#---------------------------- Configure your job ------------------------------------------------------
+DEBUG=1
+JOB_NAME="no_source_32us_window"
+INPUT_DSET="/global/cfs/cdirs/dune/users/lmlepin/neutron_source_CL_data/source_ambe_bin3/one_pmt_trig_no_source/"
+
+OUTPUT_DIR="/pscratch/sd/l/${USER}/cluster_outputs/"
+OUTPUT_FILE="${OUTPUT_DIR}${JOB_NAME}_$(date +%s)_clusters.csv"
+
+LOG_DIR="/pscratch/sd/l/${USER}/cluster_outputs/"
+LOG_FILE="${LOG_DIR}${JOB_NAME}_$(date +%s).log"
+
+SINGLE_TRIGGER=1
+USE_TRIGGER=0
+PERIOD=320
+NOHUP_OPT=0
+
+
+#----------------------------------------------------------------------------------------------------- 
+
+echo "Creating output directory"
+mkdir -p $OUTPUT_DIR
+
+echo "Creating log directory"
+mkdir -p ${LOG_DIR}
+
+if [ "$NOHUP_OPT" = 1 ]; then
+    echo "Running AmBe cluster with nohup..."
+    echo "Executing command:"
+    echo "nohup python ./charge/process_CL_AmBe_data.py --in ${INPUT_DSET} --out ${OUTPUT_FILE} --trig ${USE_TRIGGER} --st ${SINGLE_TRIGGER} --p ${PERIOD} --debug ${DEBUG} > ${LOG_FILE} 2>&1 &"
+else
+    echo "Running AmBe cluster..."
+    echo "Executing command:"
+    echo "python ./AmBe/charge/process_CL_AmBe_data_up.py --in ${INPUT_DSET} --out ${OUTPUT_FILE} --trig ${USE_TRIGGER} --st ${SINGLE_TRIGGER} --p ${PERIOD} --debug ${DEBUG}"
+    python ./charge/process_CL_AmBe_data_up.py --in ${INPUT_DSET} --out ${OUTPUT_FILE} --trig ${USE_TRIGGER} --st ${SINGLE_TRIGGER} --p ${PERIOD} --debug ${DEBUG}
+fi 
+
