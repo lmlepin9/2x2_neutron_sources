af6e16d8575f896715bd1202a1dd1c081f5dfc56
lmlepin9 <lml24d@fsu.edu>
2026-03-07 17:12:55 -0800

    MOD: New function to cluster hits, can be used for AmBe and DTG

--- utils/cluster_tools.py
+++ utils/cluster_tools.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.cluster import DBSCAN 
 
 
 def filter_hits(hits_set):
@@ -39,3 +40,78 @@ def save_to_csv(clusters_array,output_path):
     )
 
     df.to_csv(output_path)
+
+
+def cluster_hits(non_zero_charge_data,
+                 file_id,
+                 user_eps=3,
+                 user_nsamples=1):
+
+
+
+    # Initialize output dataset 
+    # This will be a dictionary so it can be saved as a csv later  
+    clusterized_hits = [] 
+    out_dataset = {
+        "clusters":None
+    }
+
+    # Get charge events and hits 
+    non_zero_charge_events = non_zero_charge_data[0]
+    non_zero_charge_hits = non_zero_charge_data[1]
+
+
+    # Setup DBScan 
+    db = DBSCAN(eps=user_eps,min_samples=user_nsamples)
+
+    for icharge in range(len(non_zero_charge_events)):
+        # Grab hits of this event 
+        test_event_hits = non_zero_charge_hits[:,0][icharge][0:non_zero_charge_events.data['nhit'][:,0][icharge]]
+
+
+        this_event_id = None
+
+        # If we include light events use this as event id for correlation with light analysis 
+        if(len(non_zero_charge_data)==3):
+            non_zero_charge_light_ev = non_zero_charge_data[2]
+            this_event_id = non_zero_charge_light_ev['id'][icharge]
+        
+        # Otherwise keep charge event id
+        else:
+            this_event_id = icharge
+
+
+
+        # Stack hits data and filter
+        hits_stack_unfiltered = np.column_stack((test_event_hits.data['x'],
+                                                 test_event_hits.data['y'],
+                                                 test_event_hits.data['z'],
+                                                 test_event_hits.data['E'],
+                                                 test_event_hits.data['Q'],
+                                                 np.ones(len(test_event_hits))*this_event_id))
+        hits_stack = filter_hits(hits_stack_unfiltered)
+
+        # Skip if no hits 
+        if(len(hits_stack)==0):
+            continue
+
+        # Run DBScan with x,y,z 
+        labels = db.fit_predict(hits_stack[:,:3])
+
+        # Add labels and append to the clusterized hits array 
+        hits_stack_label = np.column_stack((hits_stack[:,0],
+                                            hits_stack[:,1],
+                                            hits_stack[:,2],
+                                            hits_stack[:,3],
+                                            hits_stack[:,4],
+                                            np.ones(len(hits_stack))*int(this_event_id),
+                                            labels,
+                                            np.ones(len(hits_stack))*int(file_id)))
+        clusterized_hits.extend(hits_stack_label.tolist())
+
+    out_dataset["clusters"] = clusterized_hits
+
+
+    return out_dataset
+
+
